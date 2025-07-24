4044b9c9f7a730b12c87fcc24e0c75f52497550b
screwthecap1 <chanskiy777@mail.ru>
2025-07-24 12:52:14 +0400

    add some conditions

--- index.php
+++ index.php
@@ -11,12 +11,21 @@ if (!empty($_FILES['base'])) {
         $error = 'Download files with this extension is disable!';
     } elseif ($file['error'] !== UPLOAD_ERR_OK) {
         $error = 'Error while downloading file';
-    } elseif (file_exists($newFilePath)) {
-        $error = 'File with this name is already exist';
-    } elseif (!move_uploaded_file($file['tmp_name'], $newFilePath)) {
-        $error = 'Error while downloading file';
+    } elseif ($file['size'] > 8 * 1024 * 1024) {
+        $error = 'File is more than 8 MB';
     } else {
-        $result = '/uploads/' . $srcFileName;
+        $imageInfo = getimagesize($file['tmp_name']);
+        if (!$imageInfo) {
+            $error = 'There is no photo';
+        } elseif ($imageInfo[0] > 1280 || $imageInfo[1] > 720) {
+            $error = 'Wrong format of picture (more than 1280x720)';
+        } elseif (file_exists($newFilePath)) {
+            $error = 'File with this name is already exist';
+        } elseif (!move_uploaded_file($file['tmp_name'], $newFilePath)) {
+            $error = 'Error while downloading file';
+        } else {
+            $result = '/uploads/' . $srcFileName;
+        }
     }
 }
 ?>
@@ -31,11 +40,11 @@ if (!empty($_FILES['base'])) {
     <title>File Downloading</title>
 </head>
 <body>
-<?php if(!empty($error)):?>
-<?=$error?>
-<?php elseif(!empty($result)):?>
-<?=$result?>
-<?php endif;?>
+<?php if (!empty($error)): ?>
+    <?= $error ?>
+<?php elseif (!empty($result)): ?>
+    <?= $result ?>
+<?php endif; ?>
 <form action="index.php" method="post" enctype="multipart/form-data">
     <input type="file" name="base">
     <input type="submit">
